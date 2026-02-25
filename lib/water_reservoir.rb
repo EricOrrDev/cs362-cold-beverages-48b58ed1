a13e79d983c7cca4b42576686ed8c7553a97184d
Eric Orr <155666503+EricOrrDev@users.noreply.github.com>
2026-02-25 11:33:03 -0800

    added tests for vessel, item, and water reservior, all passing

--- lib/water_reservoir.rb
+++ lib/water_reservoir.rb
@@ -4,20 +4,26 @@ class WaterReservoir
   attr_accessor :current_water_volume
 
   def initialize(capacity = 10, initial_water_volume = 0)
+    if (capacity < 0)
+      raise "Capacity must be positive"
+    end
     @capacity = capacity
     @current_water_volume = initial_water_volume
   end
 
   def empty?
-    current_water_volume == 0
+    @current_water_volume == 0
   end
 
   def fill
-    current_water_volume = capacity
+    @current_water_volume = capacity
   end
 
   def drain(volume)
     self.current_water_volume -= volume
+    if self.current_water_volume < 0
+      self.current_water_volume = 0
+    end
   end
 
 end
--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,38 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+  it "has a capacity by default"do
+    water_reservoir = WaterReservoir.new
+    expect(water_reservoir.capacity).to eq(10)
+  end
+
+  it "has a capacity when specified"do
+    water_reservoir = WaterReservoir.new(100)
+    expect(water_reservoir.capacity).to eq(100)
+  end
+
+  it "raises error when negative capacity is specified"do
+    expect{WaterReservoir.new(-100)}.to raise_error("Capacity must be positive")
+  end
+
+  it "can be filled" do
+    water_reservoir = WaterReservoir.new
+    water_reservoir.fill
+    expect(water_reservoir.current_water_volume).to eq(water_reservoir.capacity)
+  end
+
+  it "can be emptied" do
+    water_reservoir = WaterReservoir.new(100)
+    water_reservoir.fill
+    water_reservoir.drain(50)
+    expect(water_reservoir.current_water_volume).to eq(50)
+  end
+
+  it "cannot have a negative current volume" do
+    water_reservoir = WaterReservoir.new(100)
+    water_reservoir.fill
+    water_reservoir.drain(500)
+    expect(water_reservoir.current_water_volume).to eq(0)
+  end
+
 end
